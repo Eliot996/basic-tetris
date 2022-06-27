c1145e382ec54f15b04970072a525c84d5e63bad
Eliot996 <math10t6@stud.kea.dk>
2022-06-27 23:07:05 +0200

    added move and freeze

--- app.js
+++ app.js
@@ -62,5 +62,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
     draw()
 
+    //timer
+    timerId = setInterval(moveDown, 1000)
+
+    function moveDown() {
+        undraw()
+        currentPosition += 10
+        draw()
+        freeze()
+    }
+
+    // freeze function using some
+    function freeze() {
+        if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
+            current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+            // start a new tetromino falling
+            random = Math.floor(Math.random()*theTetrominoes.length)
+            current = theTetrominoes[random][currentRotation]
+            currentPosition = 4
+            draw()
+        }
+    }
+
+    //
 
 })
