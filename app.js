e7dff578c92af3a10f56bad52da09cb0b16b5165
Eliot996 <math10t6@stud.kea.dk>
2022-06-27 22:33:12 +0200

    added tetrominoes

---
+++ app.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid');
+    let squares = Array.from(document.querySelectorAll('.grid div'));
+    const ScoreDisplay = document.getElementById('score');
+    const ScoreButton = document.getElementById('start-button');
+    const width = 10;
+
+    // the tetrominoes
+    const lTetromino = [
+        [1,     width+1, width*2+1, 2],
+        [width, width+1, width+2,   width*2+2],
+        [1,     width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ];
+    const skewTetromino = [
+        [width+1, width+2, width*2, width*2+1],
+        [0,       width,   width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1],
+        [0,       width,   width+1, width*2+1]
+    ];
+    const tTetromino = [
+        [1,     width,   width+1, width+2],
+        [1,     width+1, width+2, width*2+1],
+        [width, width+1, width+2, width*2+1],
+        [1,     width,   width+1, width+3]
+    ];
+    const oTetromino = [
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1]
+    ];
+    const straightTetromino = [
+        [1,     width+1, width*2+1, width*3+1],
+        [width, width+1, width+2,   width+3],
+        [1,     width+1, width*2+1, width*3+1],
+        [width, width+1, width+2,   width+3],
+
+    ];
+    const theTetrominoes = [lTetromino, skewTetromino, tTetromino, oTetromino, straightTetromino];
+
+
+    console.log(squares)
+})
